154834240a4f62c5a93bf72edd2a0354962e7904
adhaval921 <105734053+adhaval921@users.noreply.github.com>
2023-04-13 02:34:01 -0400

    assignment-3 done
    done!

---
+++ d_ahir_MyFavouriteSports/src/app/type-edit.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TypeEditPipe } from './type-edit.pipe';
+
+describe('TypeEditPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TypeEditPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
