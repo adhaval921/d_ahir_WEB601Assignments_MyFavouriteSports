b5812b950b6bd5a480bf299c04edb0f1e3e99f29
adhaval921 <105734053+adhaval921@users.noreply.github.com>
2023-04-14 02:05:18 -0400

    Assignment-3 done!

--- d_ahir_MyFavouriteSports/src/app/content-card/content-card.component.ts
+++ d_ahir_MyFavouriteSports/src/app/content-card/content-card.component.ts
@@ -1,60 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
-
+import { ContentListComponent } from '../content-list/content-list.component';
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.scss']
 })
-export class ContentCardComponent {
+export class ContentCardComponent implements OnInit {
   title = 'd_ahir_MyFavouriteSports';
-  sports: ContentList;
-  constructor() {
-    let contentitem: Content;
-    this.sports = new ContentList();
+  @Input() contentList?:Content;
+constructor(){
 
-    contentitem = {
-      id: 0,
-      title: 'basketball',
-      creator: "michael jordan",
-      description: "game of 5 players",
-      imgURL: "",
-      type: "ball game",
-      tags: ["lebron james", "stephan curry", "kobe bryant"]
-    };
-    this.sports.addContent(contentitem);
-    contentitem = {
-      id: 1,
-      title: 'baseball',
-      creator: "18th century england",
-      description: "game of 9 players",
-      imgURL: "",
-      type: "ball game",
-      tags: ["juan soto", "mike trout", "babe ruth"]
-    };
-    this.sports.addContent(contentitem);
-    contentitem = {
-      id: 2,
-      title: 'football',
-      creator: "1869 new brunswick",
-      description: "game of 11 players",
-      imgURL: "",
-      type: "ball game",
-      tags: ["ronaldo", "neymar", ]
-    };
-    this.sports.addContent(contentitem);
-    contentitem = {
-      id: 3,
-      title: 'volleyball',
-      creator: "1895 holyoke",
-      description: "game of 6 players",
-      imgURL: "",
-      type: "ball game",
-      tags: ["giba", "karch kiraly", "bartosz kurek"]
-    };
-    this.sports.addContent(contentitem);
-
+}    
+  display_card(){
+    console.log("Number of Game:-" + this.contentList?.id + "Name of Game:-" + this.contentList?.title)
   }
-
+  ngOnInit(): void {
+  
+  }
+  
 }
+// let contentitem: Content;
+    // this.sports = new ContentList();
+
+    // contentitem = {
+    //   id: 0,
+    //   title: 'basketball',
+    //   creator: "michael jordan",
+    //   description: "game of 5 players",
+    //   imgURL: "",
+    //   type: "ball game",
+    //   tags: ["lebron james", "stephan curry", "kobe bryant"]
+    // };
+    // this.sports.addContent(contentitem);
+    // contentitem = {
+    //   id: 1,
+    //   title: 'baseball',
+    //   creator: "18th century england",
+    //   description: "game of 9 players",
+    //   imgURL: "",
+    //   type: "ball game",
+    //   tags: ["juan soto", "mike trout", "babe ruth"]
+    // };
+    // this.sports.addContent(contentitem);
+    // contentitem = {
+    //   id: 2,
+    //   title: 'football',
+    //   creator: "1869 new brunswick",
+    //   description: "game of 11 players",
+    //   imgURL: "",
+    //   type: "ball game",
+    //   tags: ["ronaldo", "neymar", ]
+    // };
+    // this.sports.addContent(contentitem);
+    // contentitem = {
+    //   id: 3,
+    //   title: 'volleyball',
+    //   creator: "1895 holyoke",
+    //   description: "game of 6 players",
+    //   imgURL: "",
+    //   type: "ball game",
+    //   tags: ["giba", "karch kiraly", "bartosz kurek"]
+    // };
+    // this.sports.addContent(contentitem);
